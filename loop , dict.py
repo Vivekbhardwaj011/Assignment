472bd85441a0d064f3dde791ba007387d30c89a5
Vivekbhardwaj011 <126279592+Vivekbhardwaj011@users.noreply.github.com>
2023-02-24 23:12:36 +0530

    Add files via upload

---
+++ loop , dict.py
@@ -0,0 +1,1466 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a="hello"
+print (a)
+
+
+# In[1]:
+
+
+a=5;
+b=8;
+c=a+b;
+print(c);
+
+
+# In[2]:
+
+
+a=105;
+b=176;
+c=a+b;
+print(c);
+
+
+# In[3]:
+
+
+a=33;
+b=89;
+c=89;
+d=56;
+e=78;
+f=95;
+g=a+c+d+e+f;
+print(g);
+
+
+# In[5]:
+
+
+a=33;
+b=89;
+c=89;
+d=56;
+e=78;
+f=95;
+g=a+c+d+e+f;
+print("Addition of",a,"and",f,"=",g);
+
+
+# In[9]:
+
+
+a=33;
+b=89;
+c=89;
+d=56;
+e=78;
+f=95;
+g=342;
+h=647;
+i=648;
+j=a+b+c+d+e+f+g+h+i;
+print("Addition of",a,"and",i,"=",j);
+
+
+# In[1]:
+
+
+a=33;
+b=89;
+c=89;
+d=56;
+e=78;
+f=95;
+g=342;
+h=647;
+i=648;
+j=5563;
+k=648;
+l=a+b+c+d+e+f+g+h+i+j+k;
+print("Addition of",a,"and",k,"=",l);
+
+
+# In[9]:
+
+
+Vivek = 20 + 8j
+
+
+# In[10]:
+
+
+Type(j)
+
+
+# In[6]:
+
+
+type(j)
+
+
+# In[7]:
+
+
+type(J)
+
+
+# In[8]:
+
+
+J.real
+
+
+# In[11]:
+
+
+Vivek.real
+
+
+# In[12]:
+
+
+Vivek.imag
+
+
+# In[13]:
+
+
+Vivek.conjugate
+
+
+# In[14]:
+
+
+Vivek
+
+
+# In[15]:
+
+
+Print("My name is vivek")
+
+
+# In[16]:
+
+
+print("my name is vivek")
+
+
+# In[17]:
+
+
+print("vivek")
+
+
+# In[18]:
+
+
+print(Vivek)
+
+
+# In[20]:
+
+
+k = "bhardwaj"
+
+
+# In[21]:
+
+
+k [O]
+
+
+# In[22]:
+
+
+K[0]
+
+
+# In[23]:
+
+
+k[0]
+
+
+# In[24]:
+
+
+k[3]
+
+
+# In[26]:
+
+
+k[-5]
+
+
+# In[27]:
+
+
+k[0:3]
+
+
+# In[28]:
+
+
+k[-7:-3]
+
+
+# In[31]:
+
+
+k[0:8J:2]
+
+
+# In[34]:
+
+
+k[0:8:4]
+
+
+# In[35]:
+
+
+k[2::2]
+
+
+# In[36]:
+
+
+k[0:2:2:2]
+
+
+# In[37]:
+
+
+k[0:2:4]
+
+
+# In[38]:
+
+
+k[::-1]
+
+
+# In[40]:
+
+
+k[::-3]
+
+
+# In[41]:
+
+
+k[2:7:-1]
+
+
+# In[1]:
+
+
+K[8:0:-1]
+
+
+# In[2]:
+
+
+K[7:0:-1]
+
+
+# In[3]:
+
+
+k = "bhardwaj"
+
+
+# In[4]:
+
+
+type(k)
+
+
+# In[5]:
+
+
+k[7:0:-1]
+
+
+# In[7]:
+
+
+k[-2 : -8 :-1]
+
+
+# In[8]:
+
+
+k[-2 : :-1]
+
+
+# In[9]:
+
+
+k[::-1]
+
+
+# In[10]:
+
+
+k[::-2]
+
+
+# In[11]:
+
+
+k[-8:-1]
+
+
+# In[12]:
+
+
+k[::]
+
+
+# In[13]:
+
+
+k[-90]
+
+
+# In[14]:
+
+
+[:-90:]
+
+
+# In[15]:
+
+
+k[-90:-1]
+
+
+# In[16]:
+
+
+k[-90:-5]
+
+
+# In[18]:
+
+
+k[-90:-8]
+
+
+# In[23]:
+
+
+i = "200"
+
+
+# In[24]:
+
+
+type(i)
+
+
+# In[25]:
+
+
+i[2:1]
+
+
+# In[26]:
+
+
+2:-1
+
+
+# In[27]:
+
+
+k1 = "this is my string class"
+
+
+# In[28]:
+
+
+len(k1)
+
+
+# In[31]:
+
+
+k1.find('s')
+
+
+# In[37]:
+
+
+k1.find('1')
+
+
+# # k1.find('l')
+
+# In[38]:
+
+
+k1.find("is")
+
+
+# In[41]:
+
+
+k1.count('s')
+
+
+# In[42]:
+
+
+k1.count('l')
+
+
+# In[43]:
+
+
+k1.count('z')
+
+
+# In[44]:
+
+
+k1.count("is")
+
+
+# In[45]:
+
+
+k1.capitalize()
+
+
+# In[46]:
+
+
+k1.Upper()
+
+
+# In[47]:
+
+
+k1.upper()
+
+
+# In[48]:
+
+
+k1.lower()
+
+
+# In[49]:
+
+
+k1.tittle()
+
+
+# In[50]:
+
+
+k1.title()
+
+
+# In[51]:
+
+
+k1.capitalize()
+
+
+# In[52]:
+
+
+k
+
+
+# In[53]:
+
+
+k + 1
+
+
+# In[54]:
+
+
+k + "vivek"
+
+
+# In[55]:
+
+
+k + "vivek" + "81"
+
+
+# In[56]:
+
+
+S * 3
+
+
+# In[57]:
+
+
+k * 3
+
+
+# In[58]:
+
+
+'Vivek' *2
+
+
+# In[59]:
+
+
+'don't do copy and  paste my class'
+
+
+# In[60]:
+
+
+"don't do copy and  paste my class"
+
+
+# In[61]:
+
+
+This is my first programing class
+
+
+# In[62]:
+
+
+print(This is my first programing class)
+
+
+# In[63]:
+
+
+##This is my first programing class
+
+
+# In[70]:
+
+
+"""This is my first programing class
+i have variable declration , data type , string , int , boolean , complex number
+i have seen example of string and indexing"""
+
+
+# In[1]:
+
+
+ll
+
+
+# In[2]:
+
+
+ll= [46, 3, -45, 'try', 'vivek', 45, 45, 45, 46, 46]
+
+
+# In[3]:
+
+
+ll
+
+
+# In[4]:
+
+
+ll[::-1]
+
+
+# In[5]:
+
+
+ll.reverse()
+
+
+# In[6]:
+
+
+ll
+
+
+# In[7]:
+
+
+ll
+
+
+# In[8]:
+
+
+ll1 = [3,4,5,6,7,8,9]
+
+
+# In[9]:
+
+
+ll1
+
+
+# In[10]:
+
+
+ll1.sort()
+
+
+# In[ ]:
+
+
+
+
+
+# In[11]:
+
+
+ll
+
+
+# In[12]:
+
+
+ll1
+
+
+# In[13]:
+
+
+ll1.append(45)
+
+
+# In[14]:
+
+
+ll1
+
+
+# In[15]:
+
+
+ll.sort()
+
+
+# In[17]:
+
+
+ll2 = [34,"vivek",45,42,"bhardwaj"]
+
+
+# In[18]:
+
+
+ll4 = ["vivek" , "sharma" , "bhardwaj"]
+
+
+# In[19]:
+
+
+ll4.reverse()
+
+
+# In[20]:
+
+
+ll4.index("vivek")
+
+
+# In[21]:
+
+
+a
+
+
+# In[22]:
+
+
+ll
+
+
+# In[23]:
+
+
+a= "vivek"
+
+
+# In[24]:
+
+
+b = [3,4,5,6]
+
+
+# In[25]:
+
+
+a[0] = 'a'
+
+
+# In[26]:
+
+
+a
+
+
+# In[27]:
+
+
+b
+
+
+# In[28]:
+
+
+b[0] = 30
+
+
+# In[29]:
+
+
+b
+
+
+# In[30]:
+
+
+b[1] = 40
+
+
+# In[31]:
+
+
+b
+
+
+# In[32]:
+
+
+a
+
+
+# In[33]:
+
+
+a.replace('v' , 'b' )
+
+
+# In[34]:
+
+
+a.replace('b' , 'v')
+
+
+# In[35]:
+
+
+c = (2,3,4,5,"vivek" , 45.56, False , 45+457j , [3,4,5])
+
+
+# In[36]:
+
+
+c
+
+
+# In[37]:
+
+
+type()
+
+
+# In[39]:
+
+
+type(c)
+
+
+# In[40]:
+
+
+len(c)
+
+
+# In[41]:
+
+
+t[-1]
+
+
+# In[42]:
+
+
+c[-1]
+
+
+# In[44]:
+
+
+c[::-1]
+
+
+# In[45]:
+
+
+A
+
+
+# In[46]:
+
+
+a
+
+
+# In[47]:
+
+
+b
+
+
+# In[48]:
+
+
+c
+
+
+# In[49]:
+
+
+c[0] = 'vivek'
+
+
+# In[50]:
+
+
+t[::-1]
+
+
+# In[51]:
+
+
+c[::-1]
+
+
+# In[52]:
+
+
+t
+
+
+# In[53]:
+
+
+c
+
+
+# In[54]:
+
+
+c.reverse()
+
+
+# In[55]:
+
+
+c.count(v)
+
+
+# In[56]:
+
+
+c.count(3)
+
+
+# In[57]:
+
+
+c.index(false)
+
+
+# In[58]:
+
+
+c.index(False)
+
+
+# In[59]:
+
+
+d = {}
+
+
+# In[62]:
+
+
+type(d)
+
+
+# In[63]:
+
+
+d2 = {}
+
+
+# In[64]:
+
+
+d2 = {2,3,4,55,6}
+
+
+# In[65]:
+
+
+type(d2)
+
+
+# In[69]:
+
+
+d3 = {1234,4565,554, "vivek" , 45+45j ,34.567 , (3,4,5)}
+
+
+# In[70]:
+
+
+type(d3)
+
+
+# In[73]:
+
+
+d4 = {2,3,4,5,6,2,3,4,5,6,4,56,5,45,6,6,8,5, 'vivek', 'Vivek'}
+
+
+# In[74]:
+
+
+d4
+
+
+# In[75]:
+
+
+set(d4)
+
+
+# In[76]:
+
+
+d4 = list(set(d4))
+
+
+# In[77]:
+
+
+d4
+
+
+# In[78]:
+
+
+d5 = {234,45,65,355,22,676,565,"abc", 456,55,34}
+
+
+# In[79]:
+
+
+d5
+
+
+# In[80]:
+
+
+d5.add(4)
+
+
+# In[81]:
+
+
+d5
+
+
+# In[82]:
+
+
+d5.add('edf')
+
+
+# In[83]:
+
+
+d5
+
+
+# In[84]:
+
+
+d5.remove('edf')
+
+
+# In[85]:
+
+
+d5
+
+
+# In[86]:
+
+
+d5
+
+
+# In[1]:
+
+
+d5
+
+
+# In[2]:
+
+
+d5
+
+
+# In[3]:
+
+
+a = {}
+
+
+# In[4]:
+
+
+type(a)
+
+
+# In[8]:
+
+
+a1 = {'key' : "vivek"}
+
+
+# In[9]:
+
+
+a1
+
+
+# In[12]:
+
+
+a2 = {'name' : "mithu" , "email" : "vv@gmail.com" , "number" : 123456}
+
+
+# In[13]:
+
+
+a2
+
+
+# In[16]:
+
+
+a3 = {234 : "vivek" , '@nauto' : "hinata", True :23456}
+
+
+# In[17]:
+
+
+a3
+
+
+# In[18]:
+
+
+a3[234]
+
+
+# In[19]:
+
+
+a3[True]
+
+
+# In[20]:
+
+
+a3[1]
+
+
+# In[21]:
+
+
+a3[0]
+
+
+# In[22]:
+
+
+d3
+
+
+# In[23]:
+
+
+a3
+
+
+# In[25]:
+
+
+a3[@nauto]
+
+
+# In[28]:
+
+
+a4 = {'name':"vivek" , 'email_id' : "vivek@gmail.com" ,  'name' : "viveksharma" , 'name' : "vivekbhardwajsharma"}
+
+
+# In[29]:
+
+
+a4['name']
+
+
+# 
+
+# In[35]:
+
+
+a5 = {"company" : "skills" , "course" :["web dev" , "data science" , "javawith dsa system desgine"]}
+
+
+# In[38]:
+
+
+a5
+
+
+# In[42]:
+
+
+a5 ['course'][0]
+
+
+# In[46]:
+
+
+a6 = {"numbers" : [2,3,4,56,7] , "assignment" :(1,2,3,4,5,6,7) , "launch_date" : {28,12,14} , "class_time" : {"web_dev" : 8 , "data_science" : 8 , "javawith dsa system desgine" :7}}
+
+
+# In[51]:
+
+
+a6['class_time']
+
+
+# In[55]:
+
+
+a6['class_time']['javawith dsa system desgine']
+
+
+# 
+
+# In[56]:
+
+
+a6['mentor'] = {"vivek","deepak","akash","shani"}
+
+
+# In[57]:
+
+
+a6
+
+
+# In[59]:
+
+
+del a6['numbers']
+
+
+# In[60]:
+
+
+a6
+
+
+# In[63]:
+
+
+list(a6.keys())
+
+
+# In[62]:
+
+
+list(a6.keys))
+
+
+# In[64]:
+
+
+list(a6.values())
+
+
+# In[67]:
+
+
+list(a6.items())
+
+
+# ###### 
+
+# In[68]:
+
+
+a6
+
+
+# In[69]:
+
+
+a6.pop('assignment')
+
+
+# In[70]:
+
+
+a6
+
+
+# In[71]:
+
+
+d6.pop
+
+
+# In[83]:
+
+
+marks = 10
+if marks >= 80:
+    print("you will be a part of A0 batch")
+elif marks >= 60 and marks< 80:
+    print("you will be a part of A1 batch")
+elif marks >= 40 and marks < 60:
+    print("you will be a part of A2 batch ")
+else :
+    print("you will be a part of A3 batch ")
+
+
+# In[82]:
+
+
+81 <= 80
+
+
+# In[84]:
+
+
+marks = input("enter your marks")
+if marks >= 80:
+    print("you will be a part of A0 batch")
+elif marks >= 60 and marks< 80:
+    print("you will be a part of A1 batch")
+elif marks >= 40 and marks < 60:
+    print("you will be a part of A2 batch ")
+else :
+    print("you will be a part of A3 batch ")
+
+
+# In[87]:
+
+
+marks = int(input("enter your marks"))
+if marks >= 80:
+    print("you will be a part of A0 batch")
+elif marks >= 60 and marks< 80:
+    print("you will be a part of A1 batch")
+elif marks >= 40 and marks < 60:
+    print("you will be a part of A2 batch ")
+else :
+    print("you will be a part of A3 batch ")
+
+
+# In[ ]:
+
+
+
+
+
+# In[95]:
+
+
+price = int(input("enter price"))
+if price > 1000:
+    print("i will not purchase")
+else :
+    print("i will purchase")
+
+
+# In[ ]:
+
+
+
+
+
+# In[99]:
+
+
+price = int(input("enter price"))
+if price > 1000:
+    print("i will not purchase")
+else :
+    print("i will purchase")
+if price >5000:
+    print("this is too much high")
+elif price <2000 :
+    print('its okay')
+elif price <1500 :
+    print ("poor")
+
+
+# In[100]:
+
+
+z = [1,2,3,4,5,6,7,8,9,0,]
+
+
+# In[103]:
+
+
+z[0] +1
+
+
+# In[104]:
+
+
+z1 = []
+
+
+# In[106]:
+
+
+z1.append(z[0] +1)
+
+
+# In[107]:
+
+
+z1
+
+
+# In[108]:
+
+
+z
+
+
+# In[109]:
+
+
+z = [1,2,3,4,5,6,7,8,9,0,]
+
+
+# In[110]:
+
+
+z
+
+
+# In[111]:
+
+
+z = [1,2,3,4,5,6,7,8,9,0,]
+
+
+# In[122]:
+
+
+z1 =[]
+for i in z :
+    print(i+1)
+    z1.append(i+1)
+z1
+
+
+# In[123]:
+
+
+z = ["vivek" , "sharma" , "pwskills" , "course"]
+
+
+# In[130]:
+
+
+z1 = []
+for i in z :
+    print(i)
+    z1.append(i.upper())
+
+
+# # z1
+
+# In[131]:
+
+
+z1
+
+
+# In[133]:
+
+
+z = [1,2,3,4,5,6,7,8,8,"vivek" , "sharma" , 123,234,244,"abc"]
+
+
+# # 
+
+# In[135]:
+
+
+z1_num = []
+z2_str = []
+for i in z :
+    if type(i) == int or type(i) == float :
+        z1_num.append(i)
+    else :
+        z2_str.append(i)
+
+
+# In[136]:
+
+
+z1
+
+
+# In[138]:
+
+
+z1_num
+
+z2_str
+# In[139]:
+
+
+z2_str
+
+
+# In[ ]:
+
+
+
+
